8a7a721b169b8cda66e2082b93ec9feb7d82e795
James Hayhurst <James.A.Hayhurst@gmail.com>
2017-02-17 05:46:02 -0500

    rewrote some stuff
    - changed sanitizeInput to removeToEOL
    - started writing some junk code - remoteToToken - to hopefully remove multi-line comments

--- dump-split/sanitize.go
+++ dump-split/sanitize.go
@@ -3,17 +3,36 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"io"
 )
 
-func sanitizeInput(p Pipeline) Pipeline {
-	var output Pipeline
-	s := bufio.NewScanner(p)
+func removeToEOL(token []byte, r io.Reader) bytes.Buffer {
+	var buf bytes.Buffer
+	s := bufio.NewScanner(r)
 	go func() {
 		for s.Scan() {
-			if _, err := output.Write(bytes.TrimSpace(bytes.Split(s.Bytes(), []byte("\\!"))[0])); err != nil {
-				p.Error = err
-				return
+			if _, err := buf.Write(bytes.TrimSpace(bytes.Split(s.Bytes(), token)[0])); err != nil {
+				panic(err)
 			}
+		}
+	}()
+	return buf
+}
+
+var bufferSize = 1024 * 1024 * 16
+
+func removeToToken(start, end []byte, r io.Reader) bytes.Buffer {
+	buf := make([]byte, 0, bufferSize)
+	var readTail []byte
+	var out bytes.Buffer
+	var discard bool
+	var n int
+	var err error
+
+	go func() {
+		for {
+
+			n, err = r.Read(buf)
 		}
 	}()
 	return output
