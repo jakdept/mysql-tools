6f5a68d0789829880f1b6ec2d6d5714faa821ec8
James Hayhurst <James.A.Hayhurst@gmail.com>
2017-02-18 20:58:55 -0500

    started writing Process (function)
    - this function will take an io.Reader of sanitized input
    - it will split those lines based on what each line does
    - it willl track the current database
    - it's going to do a lot of stuff to spread out the writes per table/per db

---
+++ dump-split/process.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"io"
+)
+
+var db string
+
+// Process takes lines in via a reader, and processes them to write them out to a file
+// It removes from start (byte slice) to a end (byte slice).
+//
+// Example usage to remove go comments from source code:
+//     reader := Sanitize("/*", "*/", r)
+func Process(eol [][]byte, r io.Reader) {
+	// create a scanner that breaks on end
+	s := bufio.NewScanner(r)
+	s.Split(SplitBytesSet(eol))
+
+	for s.Scan() {
+		line := bytes.TrimSpace(s.Bytes())
+		switch {
+
+		}
+
+	}
+}
